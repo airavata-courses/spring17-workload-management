9aa6acb4915d18b6fea58d721346af2c99ca5a3a
Ajinkya Dhamnaskar <adhamnas@umail.iu.edu>
2017-02-28 23:13:45 -0500

    Removing graph db

--- GraphDB/TaskRelationships.java
+++
@@ -1,5 +0,0 @@
-
-import org.neo4j.graphdb.RelationshipType;
-public enum TaskRelationships implements RelationshipType{
-	SENDS_DATA;
-}
--- GraphDB/Tasks.java
+++
@@ -1,4 +0,0 @@
-import org.neo4j.graphdb.Label;
-public enum Tasks implements Label {
-	TASKA,TASKB,TASKC,TASKD;
-}
