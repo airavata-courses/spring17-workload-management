3cab489ef56fa4cd9b7083df2b238d119ed6a92c
Gourav Shenoy <shenoy.200@gmail.com>
2017-04-14 11:55:56 -0400

    Adding root pom for workload

--- GraphDatabase/src/main/java/org/apache/airavata/sga/graphdb/utils/States.java
+++ GraphDatabase/src/main/java/org/apache/airavata/sga/graphdb/utils/States.java
@@ -1,7 +1,15 @@
 package org.apache.airavata.sga.graphdb.utils;
+
 import org.neo4j.graphdb.Label;
+
 public enum States implements Label {
-	ENV_SETUP("ENV_SETUP"),INPUT_DATA_STAGING("INPUT_DATA_STAGING"),JOB_SUBMISSION("JOB_SUBMISSION"),MONITORING("MONITORING"),OUTPUT_DATA_STAGING("OUTPUT_DATA_STAGING");
+
+	ENV_SETUP("ENV_SETUP"),
+	INPUT_DATA_STAGING("INPUT_DATA_STAGING"),
+	JOB_SUBMISSION("JOB_SUBMISSION"),
+	MONITORING("MONITORING"),
+	OUTPUT_DATA_STAGING("OUTPUT_DATA_STAGING");
+
 	private String name;
 
 	States(String name){
