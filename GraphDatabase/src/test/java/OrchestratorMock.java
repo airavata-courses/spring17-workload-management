168bb92fe0b5c4e733dfc9a42074f0db7ab33215
Amruta <arkamat@indiana.edu>
2017-04-19 02:36:52 -0400

    Store scheduling request in  node

--- GraphDatabase/src/test/java/OrchestratorMock.java
+++ GraphDatabase/src/test/java/OrchestratorMock.java
@@ -2,12 +2,13 @@ import org.apache.airavata.sga.commons.model.SchedulingRequest;
 import org.apache.airavata.sga.graphdb.entity.State;
 import org.apache.airavata.sga.graphdb.impl.Neo4JJavaDbOperation;
 import org.apache.airavata.sga.graphdb.messaging.OrchestratorMessagePublisher;
-import org.apache.airavata.sga.graphdb.utils.Constants;
-import org.apache.airavata.sga.graphdb.utils.DummySchedulingRequest;
-import org.apache.airavata.sga.graphdb.utils.ExpTypes;
+import org.apache.airavata.sga.graphdb.utils.*;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.neo4j.graphdb.*;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
+import java.io.File;
 import java.util.*;
 
 /**
@@ -39,20 +40,52 @@ public class OrchestratorMock {
     }
 
     private static void submitJob(String experimentId, String experimentType) {
+
         try {
             Neo4JJavaDbOperation neo4JJavaDbOperation = new Neo4JJavaDbOperation();
             SchedulingRequest schedulingRequest = null;
-
+            List<String> nodesList = new ArrayList<>();
             // get scheduling request
+            experimentId = "exp";
             System.out.println("[" + Thread.currentThread().getId() + "] Getting DAG for ExperimentType: " + experimentType);
-            String results = neo4JJavaDbOperation.getDag(experimentType);
+
+            nodesList = neo4JJavaDbOperation.getCompleteDag("BIOLOGY");
+
+            Node[] arrNode = new Node[nodesList.size()];
+            File f = new File(Constants.GRAPH_DB_LOCATION);
+            GraphDatabaseFactory dbFactory = new GraphDatabaseFactory();
+            GraphDatabaseService db = dbFactory.newEmbeddedDatabase(f);
+            try (Transaction tx = db.beginTx()){
+               // db.execute("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r");
+
+                for(int i = 0; i < nodesList.size(); i++){
+                    arrNode[i] = db.createNode(Label.label(nodesList.get(i)));
+                    schedulingRequest = DummySchedulingRequest.getSchedulingRequest(Constants.fromString(nodesList.get(i)), experimentId);
+                    byte[] schdReq = SerializationUtils.convertToBytes(schedulingRequest);
+                    arrNode[i].setProperty("schedulingRequest",schdReq);
+                }
+                for(int i = 0; i < arrNode.length-1; i++){
+                    arrNode[i].createRelationshipTo(arrNode[i+1],RelationshipType.withName(experimentId));
+                }
+                tx.success();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }finally{
+                db.shutdown();
+            }
+
             System.out.println("[" + Thread.currentThread().getId() + "] Submitting Orchestrator Request for ExperimentType: " + experimentType + ", experimentId: " + experimentId);
-            schedulingRequest = DummySchedulingRequest.getSchedulingRequest(Constants.fromString(results), experimentId);
+           // String results = neo4JJavaDbOperation.getDag("BIOLOGY");
+            schedulingRequest = neo4JJavaDbOperation.getSchedulingRequestFromNode(experimentId);
+//            for (String key : resultNode.getPropertyKeys()) {
+//                System.out.println("Key: " + key + ", Value: " +  resultNode.getProperty(key));
+//            }
+           // schedulingRequest = DummySchedulingRequest.getSchedulingRequest(Constants.fromString(results), experimentId);
 
             // save the state in db
             State state = new State();
             state.setID(experimentId);
-            state.setState(results);
+            //state.setState(results);
             state.setExpType(experimentType);
 
             // submit orchestrator request
