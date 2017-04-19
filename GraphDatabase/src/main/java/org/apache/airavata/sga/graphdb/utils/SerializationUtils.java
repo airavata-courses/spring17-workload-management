708669d64fd7b3f8a15d6b43d3ceb7e82581a332
Amruta <arkamat@indiana.edu>
2017-04-19 02:33:27 -0400

    Adding serialization utils

---
+++ GraphDatabase/src/main/java/org/apache/airavata/sga/graphdb/utils/SerializationUtils.java
@@ -0,0 +1,24 @@
+package org.apache.airavata.sga.graphdb.utils;
+
+import java.io.*;
+
+/**
+ * Created by Amruta on 4/18/2017.
+ */
+public final class SerializationUtils {
+
+    public static byte[] convertToBytes(Object object) throws IOException {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutput out = new ObjectOutputStream(bos)) {
+            out.writeObject(object);
+            return bos.toByteArray();
+        }
+    }
+
+    public static Object convertFromBytes(byte[] bytes) throws IOException, ClassNotFoundException {
+        try (ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+             ObjectInput in = new ObjectInputStream(bis)) {
+            return in.readObject();
+        }
+    }
+}
