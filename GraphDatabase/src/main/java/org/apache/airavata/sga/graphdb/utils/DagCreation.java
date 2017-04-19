0c56c4c7ad45cc3af952368530091c3142da2acd
Amruta <arkamat@indiana.edu>
2017-04-19 02:34:04 -0400

    Code clean up

--- GraphDatabase/src/main/java/org/apache/airavata/sga/graphdb/utils/DagCreation.java
+++ GraphDatabase/src/main/java/org/apache/airavata/sga/graphdb/utils/DagCreation.java
@@ -4,11 +4,17 @@ import static java.lang.Math.max;
 import static java.lang.System.currentTimeMillis;
 
 import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
+import org.apache.airavata.sga.commons.model.*;
+import org.apache.airavata.sga.graphdb.impl.Neo4JJavaDbOperation;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
@@ -17,6 +23,8 @@ public class DagCreation {
 
 	private static Connection conn = null;
 	private static final Random RANDOM = new Random();
+	public static String EXP_ID;
+	private static DummySchedulingRequest dummy = new DummySchedulingRequest();
 
 	private static byte[] randomBigByteArray() {
 		byte[] array = new byte[max(248, RANDOM.nextInt(248 * 1024))];
@@ -31,47 +39,28 @@ public class DagCreation {
 		File f = new File(Constants.GRAPH_DB_LOCATION);
 		GraphDatabaseFactory dbFactory = new GraphDatabaseFactory();
 		GraphDatabaseService db = dbFactory.newEmbeddedDatabase(f);
+		Neo4JJavaDbOperation neo4JJavaDbOperation = new Neo4JJavaDbOperation();
 
 		try (Transaction tx = db.beginTx()) {
 
 			db.execute("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r");
 
-			Node taskA = db.createNode(States.ENV_SETUP);
-			Node taskB = db.createNode(States.INPUT_DATA_STAGING);
-			Node taskC = db.createNode(States.JOB_SUBMISSION);
-			Node taskD = db.createNode(States.MONITORING);
-			Node taskE = db.createNode(States.OUTPUT_DATA_STAGING);
+			Node ENV_SETUP = db.createNode(States.ENV_SETUP);
+			Node INPUT_DATA_STAGING = db.createNode(States.INPUT_DATA_STAGING);
+			Node JOB_SUBMISSION = db.createNode(States.JOB_SUBMISSION);
+			Node MONITORING = db.createNode(States.MONITORING);
+			Node OUTPUT_DATA_STAGING = db.createNode(States.OUTPUT_DATA_STAGING);
 
-			taskA.setProperty("name","taskA");
-			taskB.setProperty("name", "taskB");
-			taskC.setProperty("name", "taskC");
-			taskD.setProperty("name","taskD");
-			taskE.setProperty("name","taskE");
-		
+			ENV_SETUP.createRelationshipTo(INPUT_DATA_STAGING, ExpTypes.BIOLOGY);
+			INPUT_DATA_STAGING.createRelationshipTo(JOB_SUBMISSION, ExpTypes.BIOLOGY);
+			JOB_SUBMISSION.createRelationshipTo(OUTPUT_DATA_STAGING, ExpTypes.BIOLOGY);
 
-			taskA.createRelationshipTo(taskB, ExpTypes.BIOLOGY);
-			taskB.createRelationshipTo(taskC, ExpTypes.BIOLOGY);
-			taskC.createRelationshipTo(taskE, ExpTypes.BIOLOGY);
+			ENV_SETUP.createRelationshipTo(INPUT_DATA_STAGING, ExpTypes.CHEMISTRY);
+			INPUT_DATA_STAGING.createRelationshipTo(MONITORING, ExpTypes.CHEMISTRY);
 
-			taskA.createRelationshipTo(taskB, ExpTypes.CHEMISTRY);
-			taskB.createRelationshipTo(taskC, ExpTypes.CHEMISTRY);
-			taskC.createRelationshipTo(taskE, ExpTypes.CHEMISTRY);
+			INPUT_DATA_STAGING.createRelationshipTo(JOB_SUBMISSION, ExpTypes.PHYSICS);
+			JOB_SUBMISSION.createRelationshipTo(MONITORING, ExpTypes.PHYSICS);
 
-
-			taskA.createRelationshipTo(taskB, ExpTypes.PHYSICS);
-			taskB.createRelationshipTo(taskC, ExpTypes.PHYSICS);
-			taskC.createRelationshipTo(taskE, ExpTypes.PHYSICS);
-
-			taskA.createRelationshipTo(taskB, ExpTypes.MATHS);
-			taskB.createRelationshipTo(taskC, ExpTypes.MATHS);
-			taskC.createRelationshipTo(taskE, ExpTypes.MATHS);
-
-//			taskA.createRelationshipTo(taskB, ExpTypes.CHEMISTRY);
-//			taskB.createRelationshipTo(taskD, ExpTypes.CHEMISTRY);
-//
-//			taskB.createRelationshipTo(taskC, ExpTypes.PHYSICS);
-//			taskC.createRelationshipTo(taskD, ExpTypes.PHYSICS);
-			
 //			Result execResult = db.execute("MATCH path= (a)-[:BIOLOGY*]-(b) RETURN collect(distinct labels(b))");
 //			Result exec = db.execute("MATCH path= (a:DATA_STAGING)-[:BIOLOGY*]->(b) RETURN collect(distinct labels(b))");
 //			db.execute("MATCH path= (a:ENV_SETUP)-[:BIOLOGY*1]-(b) RETURN collect(distinct labels(b))");
@@ -87,6 +76,8 @@ public class DagCreation {
 //			for (String string : nodes) {
 //				System.out.println(string);
 //			}
+
+
 			/*
 			 * TODO:
 			 * Crate scheduler message context based on task and publish it to queue
