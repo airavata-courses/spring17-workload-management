508e336395c10ff39cbf2c232ce221bdf948679f
Ajinkya Dhamnaskar <adhamnas@umail.iu.edu>
2017-04-21 11:45:37 -0400

    Adding test NG test suit to orchestrator mock

--- GraphDatabase/src/test/java/OrchestratorMock.java
+++ GraphDatabase/src/test/java/OrchestratorMock.java
@@ -9,6 +9,9 @@ import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.io.File;
 import java.util.*;
@@ -20,29 +23,62 @@ public class OrchestratorMock {
     private static final Logger logger = LoggerFactory.getLogger(OrchestratorMock.class);
     private static OrchestratorMessagePublisher orchestratorMessagePublisher = new OrchestratorMessagePublisher();
     private static final EntityDAO DAO = new EntityDAOImpl();
+    private static final int ATTEMPTS = 5;
+    private static final int SLEEP_TIME = 4000;
+    private static final int NO_OF_EXP = 1;
 
-    public static void main(String[] args) {
+    @DataProvider(name = "ExpDataProvider")
+    public static Object[][] expData() {
+        Object[][] expData = new Object[NO_OF_EXP][2];
+        for(int i=0; i<NO_OF_EXP; i++){
+            expData[i][0] =  "exp" + UUID.randomUUID().toString().replaceAll("-", "");
+            expData[i][1] = getRandomExperimentType();
+        }
+        return expData;
 
-        // increase count here to control number of JOB-SUBMISSIONS
-        for (int i=0; i<1; i++) {
-            new Thread() {
-                @Override
-                public void run() {
-                    System.out.println("Thread: " + Thread.currentThread().getId() + " | Running Now...");
-                    //recoverJobs();
-                    submitJob("exp" + UUID.randomUUID().toString().replaceAll("-", ""), getRandomExperimentType());
-                    stop();
-                }
+    }
 
-                @Override
-                protected void finalize() throws Throwable {
-                    super.finalize();
-                    System.out.println("[" + Thread.currentThread().getId() + "] Terminating...");
-                }
-            }.start();
+//    public static void main(String[] args) {
+//
+//        // increase count here to control number of JOB-SUBMISSIONS
+//        for (int i=0; i<1; i++) {
+//            new Thread() {
+//                @Override
+//                public void run() {
+//                    System.out.println("Thread: " + Thread.currentThread().getId() + " | Running Now...");
+//                    //recoverJobs();
+//                    submitJob("exp" + UUID.randomUUID().toString().replaceAll("-", ""), getRandomExperimentType());
+//                    stop();
+//                }
+//
+//                @Override
+//                protected void finalize() throws Throwable {
+//                    super.finalize();
+//                    System.out.println("[" + Thread.currentThread().getId() + "] Terminating...");
+//                }
+//            }.start();
+//        }
+//    }
+
+    private static void recoverJobs(){
+        logger.info("Retrieving incomplete jobs from zookeeper");
+        Neo4JJavaDbOperation neo4JJavaDbOperation = new Neo4JJavaDbOperation();
+        try {
+            List<String> expIds = ZKUtils.getExpZKNodes(ZKUtils.getCuratorClient());
+            logger.info("Retrieved incomplete jobs from zookeeper : " + expIds);
+
+            for(String expId : expIds){
+                logger.info("Recovering exp : " + expIds);
+                SchedulingRequest schedulingRequest = neo4JJavaDbOperation.getSchedulingRequestFromNode(expId);
+                orchestratorMessagePublisher.publishSchedulingRequest(schedulingRequest);
+            }
+
+        } catch (Exception e) {
+            logger.error("Error recovering jobs", e);
         }
     }
 
+    @Test(invocationCount = 1, threadPoolSize = 1, description = "Test Job Submission", suiteName = "Suit1", dataProvider = "ExpDataProvider", groups = { "Exp" })
     private static void submitJob(String experimentId, String experimentType) {
         try {
             Neo4JJavaDbOperation neo4JJavaDbOperation = new Neo4JJavaDbOperation();
@@ -88,9 +124,28 @@ public class OrchestratorMock {
             // submit orchestrator request
             logger.info("[" + Thread.currentThread().getId() + "] Submitting Orchestrator Request for ExperimentType: " + experimentType + ", experimentId: " + experimentId);
             orchestratorMessagePublisher.publishSchedulingRequest(schedulingRequest);
-            System.exit(0);
+
+            ExperimentEntity experimentEntity;
+            int cnt = ATTEMPTS;
+            boolean isExecutedSuccessfully = false;
+            while(cnt>0){
+                Thread.sleep(SLEEP_TIME);
+                experimentEntity = DAO.getExperimentEntity(experimentId);
+                if(experimentEntity.getExperimentStatus().equals(Constants.ExperimentStatus.COMPLETE) ||
+                        experimentEntity.getExperimentStatus().equals(Constants.ExperimentStatus.FAILED)){
+                    isExecutedSuccessfully = true;
+                    break;
+                }
+                cnt --;
+            }
+            if(cnt == 0){
+                logger.error("Failed to execute in dedicated time. Exp : " + experimentId + ", Time : " + SLEEP_TIME/1000*ATTEMPTS + " seconds");
+                Assert.fail("Failed to execute in dedicated time. Exp : " + experimentId + ", Time : " + SLEEP_TIME/1000*ATTEMPTS + " seconds");
+            }
+            Assert.assertEquals(isExecutedSuccessfully, true);
         } catch (Exception ex) {
-            logger.error("Error running OrchestratorMock, reason: " + ex, ex);
+            logger.error("Error running OrchestratorMock",  ex);
+            Assert.fail("Error executing experiment : " + experimentId);
         }
     }
 
