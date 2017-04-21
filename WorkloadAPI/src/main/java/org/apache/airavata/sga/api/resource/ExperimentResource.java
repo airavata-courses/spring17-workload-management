383825db099e3c332a3365792da09bea20e3ca39
Gourav Shenoy <shenoy.200@gmail.com>
2017-04-21 01:42:48 -0400

    Implement REST API for Experiment

--- WorkloadAPI/src/main/java/org/apache/airavata/sga/api/resource/ExperimentResource.java
+++ WorkloadAPI/src/main/java/org/apache/airavata/sga/api/resource/ExperimentResource.java
@@ -7,6 +7,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
@@ -17,10 +18,13 @@ import org.apache.airavata.sga.graphdb.utils.OrchestratorUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Path("experiment")
 public class ExperimentResource {
 
 	private final static Logger logger = LoggerFactory.getLogger(ExperimentResource.class);
+	private ObjectMapper mapper = new ObjectMapper();
 	
 	@POST
 	@Path("launch")
@@ -31,7 +35,7 @@ public class ExperimentResource {
 			logger.info("Submitting experiment with type: {}", experimentType);
 			String experimentId = OrchestratorUtil.submitJob(experimentType);
 			logger.info("Launched experiment with Id: {}, type: {}", experimentId, experimentType);
-			response = Response.ok(experimentId);
+			response = Response.ok("{ \"experimentId\": \"" + experimentId + "\" }");
 		} catch (Exception ex) {
 			response = Response.serverError().entity("Failed to launch Experiment. Reason: " + ex.getMessage());
 		}
@@ -39,14 +43,34 @@ public class ExperimentResource {
 	}
 	
 	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getExperiments() {
+		ResponseBuilder response = null;
+		try {		
+			logger.info("Getting experiment list");
+			List<ExperimentEntity> experimentList = OrchestratorUtil.getExperimentList();
+			
+			String expJSON = mapper.writeValueAsString(experimentList);
+			logger.info("Retrieved experiment list: {}", expJSON);
+			response = Response.ok(expJSON);
+		} catch (Exception ex) {
+			response = Response.serverError().entity("Failed to get experiment list. Reason: " + ex.getMessage());
+		}
+		return response.build();
+	}
+	
+	@GET
 	@Path("{experimentId}")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response getExperiment(@PathParam("experimentId") String experimentId) {
 		ResponseBuilder response = null;
 		try {		
 			logger.info("Getting experiment with Id: {}", experimentId);
 			ExperimentEntity experiment = OrchestratorUtil.getExperiment(experimentId);
-			logger.info("Retrieved experiment: {}", experiment);
-			response = Response.ok(experiment);
+			
+			String expJSON = mapper.writeValueAsString(experiment);
+			logger.info("Retrieved experiment: {}", expJSON);
+			response = Response.ok(expJSON);
 		} catch (Exception ex) {
 			response = Response.serverError().entity("Failed to get Experiment. Reason: " + ex.getMessage());
 		}
@@ -60,8 +84,10 @@ public class ExperimentResource {
 		try {		
 			logger.info("Getting task-list for experiment with Id: {}", experimentId);
 			List<TaskStateEntity> taskList = OrchestratorUtil.getTasksForExperiment(experimentId);
-			logger.info("Retrieved task-list: {}", taskList);
-			response = Response.ok(taskList);
+			
+			String taskListJSON = mapper.writeValueAsString(taskList);
+			logger.info("Retrieved task-list: {}", taskListJSON);
+			response = Response.ok(taskListJSON);
 		} catch (Exception ex) {
 			response = Response.serverError().entity("Failed to get tasks. Reason: " + ex.getMessage());
 		}
